20b9bdd00b6edae6f40f59b447e99a6e46a846cc
Serges YAMAKO <yamakoserges@gmail.com>
2024-05-25 15:53:09 +0100

    Full Stack Sell Car Service |

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 import { SignupComponent } from './auth/auth-components/signup/signup.component';
 import { LoginComponent } from './auth/auth-components/login/login.component';
 import { AppComponent } from './app.component';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { DemoNgZorroAntdModule } from './DemoNgZorroAntModule';
 
 registerLocaleData(en);
 
@@ -25,7 +29,12 @@ registerLocaleData(en);
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NzSpinModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzButtonModule,
+    DemoNgZorroAntdModule
   ],
   providers: [
     { provide: NZ_I18N, useValue: en_US },
--- src/app/auth/auth-components/signup/signup.component.ts
+++ src/app/auth/auth-components/signup/signup.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
 
+  signupForm!:FormGroup;
+  nzSpinning: boolean = false;
+
+
+  constructor(private fb: FormBuilder){
+   this.signupForm = this.fb.group({
+      name: [null, [Validators.required]],
+      email: [null, [Validators.required,Validators.email]],
+      password: [null, [Validators.required]],
+      confirmPassword: [null, [Validators.required]],
+    })
+  }
+
+  signup(){
+    console.log(this.signupForm.value);
+  }
+  
 }
