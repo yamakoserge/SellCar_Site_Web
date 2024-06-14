3da57b2195d46dd41ad220c6bf84fe955edfa6e0
Serges YAMAKO <yamakoserges@gmail.com>
2024-06-14 18:19:39 +0100

    create login form for admin & customer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StoragesService } from './auth/auth-components/services/storages/storages.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sellcarweb';
+
+  isAdminLoggedIn: Boolean=StoragesService.isAdminLoggedIn();
+  isCustomerLoggedIn: Boolean=StoragesService.isCustomerLoggedIn();
+
+  constructor(private router: Router){}
+
+  ngOninit(){
+    this.router.events.subscribe(evet =>{
+      if(event.constructor.name === "NavigationEnd"){
+        this.isAdminLoggedIn = StoragesService.isAdminLoggedIn();
+        this.isCustomerLoggedIn = StoragesService.isCustomerLoggedIn();
+      }
+    })
+
+  }
+
+  logout(){
+    StoragesService.signout();
+    this.router.navigateByUrl("/login");
+  }
+
+
 }
