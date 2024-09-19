0e40f50ab16b2ef08a5b3c9fb3ae8c82221e1f52
Serges YAMAKO <yamakoserges@gmail.com>
2024-09-19 16:02:41 +0100

    creating & patching an update car form

--- src/app/modules/customer/components/update-car/update-car.component.ts
+++ src/app/modules/customer/components/update-car/update-car.component.ts
@@ -1,29 +1,79 @@
 import { Component } from '@angular/core';
 import { CustomerService } from '../../services/customer.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update-car',
   templateUrl: './update-car.component.html',
-  styleUrl: './update-car.component.scss'
+  styleUrls: ['./update-car.component.scss'],
 })
 export class UpdateCarComponent {
+  id: number = this.activatedRoute.snapshot.params['id'];
+  car: any;
+  existingImage: string | null = null;
 
-  id:number = this.activatedRoute.snapshot.params["id"];
-  car:any
+  listOfMarque = [
+    'BMW',
+    'AUDI',
+    'FERRARI',
+    'TESLA',
+    'VOLVO',
+    'TOYOTA',
+    'HONDA',
+    'FORD',
+    'NISSAN',
+    'HYUNDAI',
+    'LEXUS',
+    'KIA',
+    'SUZIKI',
+    'MUTSHIBUZI',
+    'AUTRE...',
+  ];
+  listOfType = ['Gasoline', 'Diesel'];
+  listOfColor = [
+    'Red',
+    'White',
+    'Blue',
+    'Black',
+    'Orange',
+    'Yellow',
+    'Grey',
+    'Silver',
+    'Autre...',
+  ];
+  listOfTransmission = ['Manuel', 'Automatic'];
+  updateCarForm: FormGroup;
+  isSpinning: boolean = false;
 
-  constructor(private service:CustomerService,
-    private activatedRoute:ActivatedRoute
-  ){}
+  constructor(
+    private service: CustomerService,
+    private activatedRoute: ActivatedRoute,
+    private fb: FormBuilder
+  ) {}
 
-  ngOnInit(){
-    this.getCars();
+  ngOnInit() {
+    this.updateCarForm = this.fb.group({
+      marque: [null, [Validators.required]],
+      name: [null, [Validators.required]],
+      type: [null, [Validators.required]],
+      transmission: [null, [Validators.required]],
+      color: [null, [Validators.required]],
+      year: [null, [Validators.required]],
+      description: [null, [Validators.required]],
+      price: [null, [Validators.required]],
+    });
+    this.getCar();
   }
 
-  getCars(){
-    this.service.getCarById(this.id).subscribe((res) =>{
+  getCar() {
+    this.service.getCarById(this.id).subscribe((res) => {
       console.log(res);
       this.car = res;
-    })
+      this.existingImage = `data:image/jpeg;base64,` + res.returnedImg;
+      this.updateCarForm.patchValue(res);
+    });
   }
+
+  updateCar() {}
 }
